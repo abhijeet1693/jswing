7a5b977035323cbce1062d34bf1ed39371348c8c
abhijeet.gavhane <abhijeet.gavhane@NTINLTP1300502.FPS.NIHILENT.com>
2019-05-19 16:52:01 +0200

    updated prac

--- SwingPrac/src/Prac.java
+++ SwingPrac/src/Prac.java
@@ -9,8 +9,8 @@ public class Prac {
 		JFrame frame = new JFrame();
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		//frame.setSize(500, 400);
-		//frame.setLocation(50, 50);
+		frame.setSize(500, 400);
+		frame.setLocation(50, 50);
 		frame.setBounds(100, 100, 500, 300);
 		frame.setTitle("My Frame");
 		ImageIcon icon = new ImageIcon("d:/ser.jpg");
